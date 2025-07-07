b9c75e62416095a299fa38cca6d0d3997571a5ed
TRANDUYTHINH <161751992+TRANDUYTHINH@users.noreply.github.com>
2025-07-07 23:27:40 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const quotes = [
+  "🌸 Bạn Đừng Buồn Nữa Nha",
+  "🌈 Miệng thiên hạ là dao hai lưỡi",
+  "☕ Đừng bận tâm nha cố lên đừng để ý lời nói của những người chê bay",
+  "🕊️ Cảm xúc của bạn là hợp lý, dù có ai nói gì đi nữa.",
+  "🌼 Bạn đủ tốt – và bạn luôn đủ đầy.",
+  "🍀 Mỗi ngày là một cơ hội để bạn bắt đầu lại và yêu thương chính mình.",
+  "Tác giả kêu là :  Yen Anh!"
+];
 
+function showHealing() {
+  const quote = document.getElementById("quote");
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  quote.style.opacity = 0;
+  setTimeout(() => {
+    quote.innerText = randomQuote;
+    quote.style.opacity = 1;
+  }, 300);
+}
